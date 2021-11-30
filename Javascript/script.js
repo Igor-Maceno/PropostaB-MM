3d558f47e7ab6d1b0523617c5addc3cd23e3653a
Igor-Maceno <igorlopesmaceno17@gmail.com>
2021-11-30 05:15:51 -0300

    Layout 01 do projeto

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,10 +5,23 @@ if(localStorage.getItem('token') == null){
 let userLogado = JSON.parse(localStorage.getItem('userLogado'));
 
 let logado = document.querySelector('#logado');
-logado.innerHTML = "Olá, " + userLogado.nome;
+logado.innerHTML = userLogado.nome;
 
 function sair(){
     localStorage.removeItem('token');
     localStorage.removeItem('userLogado');
     window.location.href='login.html'
 }
+'use strict'
+
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => document.getElementById('modal')
+    .classList.remove('active')
+
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
